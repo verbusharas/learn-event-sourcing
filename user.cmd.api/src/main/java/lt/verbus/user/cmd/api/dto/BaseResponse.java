d71f2b3ade6d24d7268719e8b0818b6a3378eec2
Sarunas Verbus <sarunas@verbus.lt>
2021-05-30 19:34:08 +0300

    add REST controller for user registration

---
+++ user.cmd.api/src/main/java/lt/verbus/user/cmd/api/dto/BaseResponse.java
@@ -0,0 +1,10 @@
+package lt.verbus.user.cmd.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class BaseResponse {
+    private String message;
+}
---
+++ user.cmd.api/src/main/java/lt/verbus/user/cmd/api/dto/RegisterUserResponse.java
@@ -0,0 +1,9 @@
+package lt.verbus.user.cmd.api.dto;
+
+public class RegisterUserResponse extends BaseResponse{
+
+    private String id;
+    public RegisterUserResponse(String message) {
+        super(message);
+    }
+}
